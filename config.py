5cba1aca466e4e975b51cae68d0afaa8243f2795
Jegaa1 <121353390+Jegaa1@users.noreply.github.com>
2023-04-05 01:34:09 +0530

    Update config.py

--- config.py
+++ config.py
@@ -11,7 +11,7 @@ from logging.handlers import RotatingFileHandler
 from dotenv import load_dotenv
 load_dotenv()
 #Bot token @Botfather
-TG_BOT_TOKEN = os.environ.get("TG_BOT_TOKEN", "6059610280:AAEoR1vdoWvv3vLAdwMKX1Qk4b_RebslKZQ")
+TG_BOT_TOKEN = os.environ.get("TG_BOT_TOKEN", "6088485689:AAGwgRZELBOUNZvvxifJYhYt1LtHUasXypU")
 
 #Your API ID from my.telegram.org
 APP_ID = int(os.environ.get("APP_ID", "1923471"))
@@ -29,8 +29,8 @@ OWNER_ID = int(os.environ.get("OWNER_ID", "880087645"))
 PORT = os.environ.get("PORT", "8080")
 
 #Database 
-DB_URI = os.environ.get("DATABASE_URL", "mongodb+srv://Erichdaniken:Erichdaniken@cluster0.vhu3d.mongodb.net/?retryWrites=true&w=majority")
-DB_NAME = os.environ.get("DATABASE_NAME", "myserialbot")
+DB_URI = os.environ.get("DATABASE_URL", "mongodb+srv://video:video@rishi.fzpls33.mongodb.net/?retryWrites=true&w=majority")
+DB_NAME = os.environ.get("DATABASE_NAME", "asuranj")
 
 #force sub channel id, if you want enable force sub
 FORCE_SUB_CHANNEL = int(os.environ.get("FORCE_SUB_CHANNEL", "-1001719990269"))
