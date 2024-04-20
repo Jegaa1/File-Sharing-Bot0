0ef690eb70c9cf368a4e83b80794b53b908270dc
Codaangi <121353390+Jegaa1@users.noreply.github.com>
2024-04-21 00:13:12 +0530

    Update link_generator.py

--- plugins/link_generator.py
+++ plugins/link_generator.py
@@ -35,9 +35,10 @@ async def batch(client: Client, message: Message):
 
     string = f"get-{f_msg_id * abs(client.db_channel.id)}-{s_msg_id * abs(client.db_channel.id)}"
     base64_string = await encode(string)
-    link = f"https://telegram.me/{client.username}?start={base64_string}"
+    link = f"https://tamilserialbot.jasurun.workers.dev?start={base64_string}"
+    #short_link = await get_shortlink(f"https://tamilserialbot.jasurun.workers.dev?start={base64_string}")
     reply_markup = InlineKeyboardMarkup([[InlineKeyboardButton("🔁 Share URL", url=f'https://telegram.me/share/url?url={link}')]])
-    await second_message.reply_text(f"<b>Link</b>: {link}", quote=True, reply_markup=reply_markup)
+    await second_message.reply_text(f"<b>8 into 1: {link}</b>", quote=True, reply_markup=reply_markup)
 
 
 @Bot.on_message(filters.private & filters.user(ADMINS) & filters.command('genlink'))
@@ -55,6 +56,7 @@ async def link_generator(client: Client, message: Message):
             continue
 
     base64_string = await encode(f"get-{msg_id * abs(client.db_channel.id)}")
-    link = f"https://telegram.me/{client.username}?start={base64_string}"
+    link = f"https://tamilserialbot.jasurun.workers.dev?start={base64_string}"
+    #short_link = await get_shortlink(f"https://telegram.me/{client.username}?start={base64_string}")
     reply_markup = InlineKeyboardMarkup([[InlineKeyboardButton("🔁 Share URL", url=f'https://telegram.me/share/url?url={link}')]])
-    await channel_message.reply_text(f"<b>Link</b>: {link}", quote=True, reply_markup=reply_markup)
+    await channel_message.reply_text(f"<b>{file_name} ~ [⏰ {duration}] - {file_size}\n\nLink: {short_link}</b>", quote=True, reply_markup=reply_markup)
