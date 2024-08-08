0d88eac46bcf23cbf4202c618146e0b29616daa2
Codaangi <121353390+Jegaa1@users.noreply.github.com>
2024-08-08 18:56:55 +0530

    Update config.py

--- config.py
+++ config.py
@@ -42,7 +42,7 @@ TUT_VID = os.environ.get("TUT_VID","https://t.me/demoshort/50")
 #force sub channel id, if you want enable force sub
 FORCE_SUB_CHANNEL = int(os.environ.get("FORCE_SUB_CHANNEL", "-1002092476960"))
 
-TG_BOT_WORKERS = int(os.environ.get("TG_BOT_WORKERS", "8"))
+TG_BOT_WORKERS = int(os.environ.get("TG_BOT_WORKERS", "60"))
 
 
 
